7fffd8d0ba31844f436856c875b1e2916352f6e3
Johaney-s <johana.supikova@seznam.cz>
2024-08-01 10:01:58 +0200

    Changes for including Python scripts
    * ~/.netrc file should be filled in the docker compose
    * get_new_list.py stores files to a constant file

--- gist/check-new-register-stac.sh
+++ gist/check-new-register-stac.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 # This script is meant for being called regularly by cron
 
+mkdir -p /var/tmp/sentinel/
+
 SCRIPTNAME="`basename -s .sh $0`"
-LOCK="/var/tmp/$SCRIPTNAME.lock"
-VARPREFIX="/var/tmp/stac-new-`date +%Y-%m-%d_%H:%M`"
+LOCK="/var/tmp/sentinel/$SCRIPTNAME.lock"
+LIST="/var/tmp/sentinel/gen_new_list_processed.txt"
 
 if [ -e $LOCK ]; then
-	1>&2 printf "Exitting: Lock file exists: $LOCK\n\"$SCRIPTNAME\" is only meant to be run once at a time.\n\n"
+	1>&2 printf "Exiting: Lock file exists: $LOCK\n\"$SCRIPTNAME\" is only meant to be run once at a time.\n\n"
 	exit 1
 else
 	touch $LOCK
 	trap "rm \"$LOCK\"" EXIT
 fi
 
-bash ./gen_new_list.sh -e -v https://dhr1.cesnet.cz > "$VARPREFIX"
+echo "Fetching products"
+python3 gen_new_list.py  # todo - check paths
+if [ $? -ne 0 ]; then
+  echo "Failed to fetch products."
+  exit 1
+fi
+echo "Fetched $(wc -l < $LIST) products"
 
-echo "Spoustim ./register-stac.sh"
-
-#Vyvoreni .netrc2 pro overeni pres curl pro katalog
-echo "machine $CATALOG" >> /root/.netrc2     && echo "login $LOGIN2" >> /root/.netrc2     && echo "password $PASSWORD2" >> /root/.netrc2
-
-cat "$VARPREFIX" | while read id; do
-	 ./register-stac.sh $id
+cat "$LIST" | while read id; do
+	python3 register_stac.py -p -i $id  # todo - check paths
 done
-echo "Dokoncil jsem script!"
